a6ed11bfd4b27adee865de4331657e910b8931a1
ZerterCodes <jared.coombs12345@gmail.com>
2016-12-14 22:51:05 +1000

    Update Details.js

--- code/bots/Details.js
+++ code/bots/Details.js
@@ -1,3 +1,9 @@
 on.event(jsiv.bot("load",function() { 
     console.log(colors.data("Done Loading")) 
 });
+         
+on.event(jsiv.bot("core",staff-core(embed)) {
+    event.create(new.event) {
+        jsiv.bot() :: logData();
+    } => only(if){ bot.event equalTo | if(isTrue?).accept }
+};
